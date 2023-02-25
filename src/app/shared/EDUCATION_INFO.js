15300ff4090dcce1003917dae0d79dd53a9a9b97
Jeremy Jeremiah <jeremyjeremiah587@gmail.com>
2023-02-25 11:52:50 -0700

    Upgraded Education Info file

--- src/app/shared/EDUCATION_INFO.js
+++ src/app/shared/EDUCATION_INFO.js
@@ -3,23 +3,25 @@ export const EDUCATION_INFO = [
         id: 0,
         campsiteId: 0,
         rating: 5,
-        text: 'Learn data structures and algorithms to build robust applications with Python and SQL. \n Use DevOps tools and deploy to the Amazon AWS, Microsoft Azure and Google clouds.',
+        text: ('Software Development Methodologies & Lifecycle, \n Team Development with Git, \n Docker Images and Containers, \n The Django Web Framework, \n Docker Publishing, \n Advanced Docker Topics, \n Cloud Computing with Amazon Web Services, CI/CD Python Testing Frameworks, Unit & Integration Testing, Cloud Computing and Serverless Technologies with Google Cloud Advanced DevOps, Package Management Advanced DevOps, Linux & Network Tools Kubernetes & Container Orchestration Cloud Computing with Azure '
+        ),
         author: 'Mark Thompson',
         date: '2022-12-25T15:30Z'
     },
     {
-        id: 6,
+        id: 1,
         campsiteId: 1,
         rating: 5,
-        text: 'Learn data structures and algorithms to build robust applications with Python and SQL. \n Use DevOps tools and deploy to the Amazon AWS, Microsoft Azure and Google clouds. Complete your portfolio project and recieve the Honors Certificate',
+        text: ('Software Development Methodologies & Lifecycle, \n Team Development with Git, \n Docker Images and Containers, \n The Django Web Framework, \n Docker Publishing, \n Advanced Docker Topics, \n Cloud Computing with Amazon Web Services, CI/CD Python Testing Frameworks, Unit & Integration Testing, Cloud Computing and Serverless Technologies with Google Cloud Advanced DevOps, Package Management Advanced DevOps, Linux & Network Tools Kubernetes & Container Orchestration Cloud Computing with Azure '
+        ),
         author: 'McKenzie Smith',
         date: '2023-02-18T13:12Z'
     },
     {
-        id: 10,
+        id: 2,
         campsiteId: 2,
         rating: 5,
-        text: 'Course Description Comming Soon',
+        text: 'Introductory Concepts, Introduction to Bootstrap, Introduction to Python Programming, Conditionals, While Loops, and Input, For Loops, Python Functions, Modules and Packages, The Random Module, Introduction to Data Structures, Lists, Strings, Dictionaries, Tuples, Sets, Object-Oriented Programming (OOP), Introduction to Abstract Data Types, Linked Lists, Stacks, Queues, Introduction to Algorithms, Searching Algorithms, Sorting Algorithms, Big O Notation ',
         author: 'Mike the Snowman',
         date: '2022-02-18T22:12Z'
     },
@@ -29,6 +31,16 @@ export const EDUCATION_INFO = [
         campsiteId: 3,
         rating: 4,
         text: 'Course Description Comming Soon',
+        author: 'Markell Rich',
+        date: '2022-06-17T03:33Z'
+    }
+    ,
+    
+    {
+        id: 16,
+        campsiteId: 4,
+        rating: 4,
+        text: 'Docker Installation, Data Modeling, Integrity Constraints, Tables, Docker, Postgres & psql, Getting to Know SQL, Database Manipulation, Database Queries, Database Queries: Aggregations, Multi-Table Queries: Set Operations & Joins, Multi-Table Queries: Subqueries & WITH Queries, Python Packages and Virtual Environments,Migrations,Object Relational Mapping (ORM), Application Programming Interface (API), Backend App Development, Advanced SQL Techniques, Optimization, Administration, Introduction to Data Visualization, Visualizing Data Files ',
         author: 'Markell Rich',
         date: '2022-06-17T03:33Z'
     }
