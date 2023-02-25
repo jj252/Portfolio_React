423b91df83303e4a711aaa37b84a1a0639e5b94a
Jeremy Jeremiah <jeremyjeremiah587@gmail.com>
2023-02-25 12:24:40 -0700

    Upgraded Education Info

--- src/app/shared/EDUCATION_INFO.js
+++ src/app/shared/EDUCATION_INFO.js
@@ -27,7 +27,7 @@ export const EDUCATION_INFO = [
     },
     
     {
-        id: 15,
+        id: 3,
         campsiteId: 3,
         rating: 4,
         text: 'Course Description Comming Soon',
@@ -37,10 +37,30 @@ export const EDUCATION_INFO = [
     ,
     
     {
-        id: 16,
+        id: 4,
         campsiteId: 4,
         rating: 4,
         text: 'Docker Installation, Data Modeling, Integrity Constraints, Tables, Docker, Postgres & psql, Getting to Know SQL, Database Manipulation, Database Queries, Database Queries: Aggregations, Multi-Table Queries: Set Operations & Joins, Multi-Table Queries: Subqueries & WITH Queries, Python Packages and Virtual Environments,Migrations,Object Relational Mapping (ORM), Application Programming Interface (API), Backend App Development, Advanced SQL Techniques, Optimization, Administration, Introduction to Data Visualization, Visualizing Data Files ',
+        author: 'Markell Rich',
+        date: '2022-06-17T03:33Z'
+    }
+    ,
+    
+    {
+        id: 5,
+        campsiteId: 5,
+        rating: 4,
+        text: 'Pure Functions and Side Effects, Declarative vs. Imperative, The Virtual DOM, What is JSX, Components and Props, Composing with Components, State, Hooks, Single Page Applications & React Router, More React Router, JS: Ternary Operator, The useEffect Hook, CSS Animations & React Spring, JavaScript Computed Property Names & Spready Syntax, Forms in React, Formik, MVC, Flux, Reducers and Redux, Redux: Slices and Selectors, Redux: Dispatching Actions & Adding Middleware, Client-Server Communication, Fetch and createAsycThunk, Fetching and Async Redux Practice, React Web App Deployment with Firebase ',
+        author: 'Markell Rich',
+        date: '2022-06-17T03:33Z'
+    }
+    ,
+    
+    {
+        id: 6,
+        campsiteId: 6,
+        rating: 4,
+        text: 'Networking Fundamentals, Basic Ethernet LANs, Ethernet LANs: Design, VLANs, and Troubleshooting, IP Version 4 Addressing and Subnetting, Implementing IPv4, IPv4 Design and Troubleshooting, IPv4 Services: ACLs and NAT, IP Version 6,  IPv6 Addressing and Subnetting, Implementing IPv6 Addressing on Routers ,Implementing IPv6 Addressing on Hosts ,Implementing IPv6 Routing , Network Device Management ,Device Management Protocols ,Device Security Features',
         author: 'Markell Rich',
         date: '2022-06-17T03:33Z'
     }
